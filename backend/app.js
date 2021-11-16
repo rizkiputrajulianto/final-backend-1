8f8135675a462d302e70b96ded81874ec3fc844d
Rizki Putra Julianto <rizkiputrajulianto111@gmail.com>
2021-11-16 19:51:45 +0700

    install cors

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const {sequelize, Sequelize} = require('./src/models');
 const router = require('./src/routes');
+const cors = require('cors');
 
 // sequelize.sync({force: true});
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
